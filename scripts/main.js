77bef5a89eeb3ca6571ee67cf1cc9d37cdc4ab43
Jacob Eckert <jweckert17@gmail.com>
2020-07-21 15:08:01 -0500

    factored out past entries nav into separate js module

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { JournalEntryForm } from './JournalEntryForm.js';
 import { JournalEntryList } from './JournalEntryList.js';
+import { JournalEntryNav } from './JournalEntryNav.js';
 
 JournalEntryForm();
 JournalEntryList();
+JournalEntryNav();
