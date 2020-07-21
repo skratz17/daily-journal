9bad9633d92a76b746c3591987642f056e5a43f6
Jacob Eckert <jweckert17@gmail.com>
2020-07-21 14:39:53 -0500

    styled journal entries

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -7,8 +7,8 @@ export const JournalEntry = journalEntry => {
     <article id="entry--${escape(id)}" class="journal-entry">
       <p class="journal-entry__date">${escape(date)}</p>
       <div class="journal-entry__concept-mood-wrapper">
-        <p class="journal-entry__concept">${escape(concept)}</p>
-        <p class="journal-entry__mood">${escape(mood)}</p>
+        <p class="journal-entry__concept">Concepts: ${escape(concept)}</p>
+        <p class="journal-entry__mood">Mood: ${escape(mood)}</p>
       </div>
       <p class="journal-entry__entry">${escape(entry)}</p>
     </article>
