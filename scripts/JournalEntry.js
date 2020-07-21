a034ae1759d517a076c058a2b5ca2d243a4a6e45
Jacob Eckert <jweckert17@gmail.com>
2020-07-21 14:58:31 -0500

    created mapping of mood integer value to emoji in journalentry.js

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -3,12 +3,15 @@ import escape from './escapeHTML.js';
 export const JournalEntry = journalEntry => {
   const { id, date, concept, entry, mood } = journalEntry;
 
+  const moodEmojis = ['👿', '🤬', '😡', '😭', '😕', '🙃', '🙂', '😀', '😁', '🤠'];
+  const moodEmoji = moodEmojis[mood - 1];
+
   return `
     <article id="entry--${escape(id)}" class="journal-entry">
       <p class="journal-entry__date">${escape(date)}</p>
       <div class="journal-entry__concept-mood-wrapper">
         <p class="journal-entry__concept">Concepts: ${escape(concept)}</p>
-        <p class="journal-entry__mood">Mood: ${escape(mood)}</p>
+        <p class="journal-entry__mood">Mood: ${moodEmoji}</p>
       </div>
       <p class="journal-entry__entry">${escape(entry)}</p>
     </article>
