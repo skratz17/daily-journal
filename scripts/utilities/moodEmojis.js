7f8581c9be147240bb34f56f2ef3d8ebeb45eceb
Jacob Eckert <jweckert17@gmail.com>
2020-07-22 14:44:26 -0500

    documentation

--- scripts/utilities/moodEmojis.js
+++ scripts/utilities/moodEmojis.js
@@ -1,6 +1,10 @@
 const moodEmojis = ['👿', '🤬', '😡', '😭', '😕', '🙃', '🙂', '😀', '😁', '🤠'];
 const DEFAULT_MOOD_VALUE = 6;
 
+/**
+ * Given a value, attempt to parse it as an integer and return the emoji corresponding to that integer index in the moodEmojis array.
+ * @param {any} value Value to get emoji for
+ */
 export const getMoodEmoji = value => {
   value = parseInt(value);
 
@@ -12,6 +16,17 @@ export const getMoodEmoji = value => {
   return moodEmojis[value];
 };
 
+/**
+ * Get the default mood emoji.
+ */
 export const getDefaultMoodEmoji = () => moodEmojis[DEFAULT_MOOD_VALUE];
+
+/**
+ * Get the integer mood value of the default mood emoji.
+ */
 export const getDefaultMoodValue = () => DEFAULT_MOOD_VALUE;
+
+/**
+ * Get the count of emojis in the moodEmojis array.
+ */
 export const getEmojisCount = () => moodEmojis.length;
