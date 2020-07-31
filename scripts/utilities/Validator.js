8bd19133fefb6f893bcde768d600f30020336ac2
Jacob Eckert <jweckert17@gmail.com>
2020-07-31 11:41:14 -0500

    rewrote most of Validator to make it way more straightforward, removed some code that did slightly minimize dom manipulation but also cluttered the code

--- scripts/utilities/Validator.js
+++ scripts/utilities/Validator.js
@@ -1,9 +1,6 @@
-/**
- * Class implementing various functions related to validating values of an object.
- * Extend it to create a validator for an arbitrary type of object
- */
 export class Validator {
-  propertiesToValidate = {}
+  validators = []
+  errors = []
 
   /**
    * 
@@ -13,62 +10,27 @@ export class Validator {
    */
   addValidatorToProperty = (validator, errorMessage, ...propertyNames) => {
     propertyNames.forEach(propertyName => {
-      if(!this.propertiesToValidate[propertyName]) {
-        this.propertiesToValidate[propertyName] = [];
-      }
-      this.propertiesToValidate[propertyName].push({ validator, errorMessage });
+      const validationInfo = { propertyName, validator, errorMessage };
+      this.validators.push(validationInfo);
     });
-  }
-
-  /**
-   * Return true if the last time this.validate was called, all validation tests passed.
-   */
-  isValid = () => {
-    const properties = this.propertiesToValidate;
-
-    for(const propertyName of Object.keys(properties)) {
-      for(const validationInfo of properties[propertyName]) {
-        if(!validationInfo.isValid) return false;
-      }
-    }
-    return true;
-  }
-
-  /**
-   * Get an object of the form { <propertyName>: [ array of error message strings for all errors detected for this property on last validate() ]}
-   */
-  getErrors = () => {
-    const properties = this.propertiesToValidate;
-    const errors = {};
-
-    Object.keys(properties).forEach(propertyName => {
-      errors[propertyName] = 
-        properties[propertyName]
-          .filter(validationInfo => !validationInfo.isValid)
-          .map(validationInfo => validationInfo.errorMessage);
-    });
-
-    return errors;
   }
 
   /**
    * Given an object, run all validator functions defined for each property of the object. 
    * @param {Object} object The object that you want to validate.
-   * @returns Object of the form { isValid: <boolean value representing whether all tests were passed or not>, errors: { object mapping property names to array of error messages for detected errors for that property }}
+   * @returns {Array} Array of objects representing validations failed - each object will be of the form { propertyName {String}, errorMessage {String} }
    */
   validate = object => {
-    const properties = this.propertiesToValidate;
+    this.errors = [];
 
-    Object.keys(properties).forEach(propertyName => {
+    this.validators.forEach(validationInfo => {
+      const { propertyName, validator, errorMessage } = validationInfo;
       const valueToValidate = object[propertyName];
-      properties[propertyName].forEach(validationInfo => {
-        validationInfo.isValid = validationInfo.validator(valueToValidate);
-      });
+      if(!validator(valueToValidate)) {
+        this.errors.push({ propertyName, errorMessage });
+      }
     });
 
-    return {
-      isValid: this.isValid(),
-      errors: this.getErrors()
-    };
+    return this.errors.slice();
   }
 }
